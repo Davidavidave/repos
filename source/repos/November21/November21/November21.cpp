8181d7c497d49c67cd1cfd38cc4a68ff7a8eef88
Davidavidave <stephendavealvarezblanza@gmail.com>
2024-11-22 17:11:18 +0800

    new

---
+++ source/repos/November21/November21/November21.cpp
@@ -0,0 +1,16 @@
+// November21.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    std::cout << "Hello World!\n";
+    std::cout << "Can you enter your name\n";
+      cin >> name;
+
+}
+
